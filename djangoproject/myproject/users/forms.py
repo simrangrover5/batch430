966b691abd2492ca5ea4808c7cf640e0e1f765b3
simrangrover5 <simrangrover5@gmail.com>
2020-01-24 11:45:25 +0530

    django code for models

--- djangoproject/myproject/users/forms.py
+++ djangoproject/myproject/users/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class Login(forms.Form):
+    email = forms.EmailField(label="Email")
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class Signup(forms.Form):
+    username = forms.CharField(max_length=100)
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     
--- djangoproject/myproject/users/views.py
+++ djangoproject/myproject/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import Login
+from .forms import Login,Signup
 # Create your views here.
 def index(request):
     return render(request,"users/index.html")
@@ -9,3 +9,17 @@ def index(request):
 def login(request):
     form = Login()
     return render(request,"users/login.html",{'f':form})
+
+def afterlogin(request):
+    form = Login(request.POST)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        return HttpResponse("Email : {} and password {}".format(email,password))
+    else:
+        error = "Invalid form..."
+        return render(request,"users/login.html",{'error':error})
+
+def signup(request):
+    form = Signup()
+    return render(request,"users/signup.html",{'f':form})
